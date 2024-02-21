7b22459834583ec08873de713544d3d94ba7fc09
Максим Блинов <126991331+maskon@users.noreply.github.com>
2024-02-21 09:29:35 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,83 @@
+const question = document.getElementById('question')
+const questionNumber = document.getElementById('question--number')
+const quizOptionAll = document.getElementById('quiz__options')
+const btn = document.getElementById('btn')
+const quisItem = document.getElementById('quis__item')
+const quizNum = document.getElementById('quiz__num')
+const quizLength = document.getElementById('quiz__length')
+const quizSmile = document.getElementById('quiz__smile')
+
+let count = 0
+let total = 0
+let correctAnswer = 0
+
+blocksBtn()
+renderHTML()
+questionСounter()
+renderTotal()
+
+btn.addEventListener('click', () => {
+    quizOptionAll.innerHTML = '' 
+    if (count < (data.length - 1)) {
+        count++
+        renderHTML()
+        questionСounter()
+        renderTotal()
+        total > 0 ? correctAnswer++ : correctAnswer
+    } else {
+        questionNumber.innerHTML = ''
+        quizSmile.style.display = 'block'
+        question.innerHTML = `Тест окончен ваш результат ${correctAnswer} из ${data.length}`
+        quisItem.innerHTML = ''
+    }
+})
+
+function renderHTML() {
+    for (let i = 0; i < data[count].quiz__option.length; i++) {
+        quizOptionAll.insertAdjacentHTML('beforeend', `
+            <div class="quiz__option">
+                <div class="quiz__text">${data[count].quiz__option[i]}</div>
+            </div>`)
+    }
+}
+
+function questionСounter() {
+    question.innerHTML = data[count].question
+    questionNumber.innerHTML = count + 1 + '.'
+    quizNum.innerHTML = count + 1
+    quizLength.innerHTML = data.length
+}
+
+function renderTotal() {
+    quizOptionAll.addEventListener('click', (e) => {
+        const quizAll = quizOptionAll.querySelectorAll('.quiz__text')
+        quizAll.forEach(item => item.classList.remove('quiz__text__active'))
+        total = 0
+        if (e.target.classList.contains('quiz__text')) {
+            e.target.classList.add('quiz__text__active')
+            btn.disabled = false
+            btn.style.opacity = '1'
+            btn.style.cursor = 'pointer'
+            if (e.target.innerText === 'all of the above'&&
+                e.target.classList.contains('quiz__text__active')|| 
+                e.target.innerText === 'string'|| 
+                e.target.innerText === '==='|| 
+                e.target.innerText === 'log()'|| 
+                e.target.innerText === 'for'|| 
+                e.target.innerText === 'typeof()'|| 
+                e.target.innerText === 'parseInt()'|| 
+                e.target.innerText === 'push()'|| 
+                e.target.innerText === '+'|| 
+                e.target.innerText === 'Math.floor(Math.random()  10)') {     
+                total++
+            }
+        }
+    })
+    blocksBtn()
+}
+
+function blocksBtn() {
+    btn.disabled = true
+    btn.style.opacity = '0.5'
+    btn.style.cursor = 'default'
+}
---
+++ js/data.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        question: 'Каким образом можно объявить переменную в JavaScript?',
+        quiz__option: ['var', 'let', 'const', 'all of the above']
+
+    },
+              
+    {
+        question: 'Какой тип данных у переменной, содержащей строку "Hello World"?',
+        quiz__option: [' number', 'boolean', 'string']
+    },
+              
+    {
+        question: 'Каким образом можно объявить переменную в JavaScript?',
+        quiz__option: [' ==', '===', '=', '!==']
+    },
+              
+    {
+        question: 'Какой метод используется для вывода информации в консоль в JavaScript?',
+        quiz__option: ['console.write()', 'alert()', 'log()', ' print()']
+    },
+              
+    {
+        question: 'Какой цикл используется для выполнения кода определенное количество раз в JavaScript?',
+        quiz__option: ['for', 'while', 'do-while', 'forEach']
+    },
+              
+    {
+        question: 'Каким образом можно проверить тип данных переменной в JavaScript?',
+        quiz__option: ['typeof()', 'typeOf()', 'checkType()', 'getType()']
+    },
+              
+    {
+        question: 'Какой метод используется для преобразования строки в число в JavaScript?',
+        quiz__option: ['toNumber()', 'parseInt()', 'parseNumber()', 'convertToNumber()']
+    },
+              
+    {
+        question: ' Как добавить элемент в конец массива в JavaScript?',
+        quiz__option: ['push()', 'add()', 'append()', 'insert()']
+    },
+              
+    {
+        question: 'Какой оператор используется для объединения двух или более строк в JavaScript?',
+        quiz__option: ['+', '&', '||', '$']
+    },
+              
+    {
+        question: 'Как получить случайное целое число в определенном диапазоне в JavaScript?',
+        quiz__option: ['Math.floor(Math.random()  10)', 'Math.random()  10', 'Math.round(Math.random()  10)', 'Math.ceil(Math.random()  10)']
+    },           
+]
